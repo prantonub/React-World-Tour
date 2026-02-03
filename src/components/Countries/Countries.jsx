8b1c5aa98bbf159d073913af7944cc14c321593b
prantonub <prantonub.cse23@gmail.com>
2026-02-03 15:37:55 +0600

    react world tour

---
+++ src/components/Countries/Countries.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import Country from "../Country/Country";
+
+const Countries = () => {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://restcountries.com/v3.1/all?fields=name,cca3,capital,region,subregion,population,flags,languages,currencies",
+    )
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
+  }, []);
+
+  return (
+    <div>
+      <h3>Countries: {countries.length}</h3>
+
+      {countries.map((country) => (
+        <Country key={country.cca3} country={country} />
+      ))}
+    </div>
+  );
+};
+
+export default Countries;
